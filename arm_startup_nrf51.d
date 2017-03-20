10a65f5f274a3f04bda4237e721257a2fad68794
pauksk <nlh.lorros0@gmail.com>
2017-03-20 02:16:03 +0100

    Project cleanup

--- arm_startup_nrf51.d
+++
@@ -1,1 +0,0 @@
-.\arm_startup_nrf51.o: C:\components\toolchain\arm\arm_startup_nrf51.s
--- segger_rtt.d
+++
@@ -1,5 +0,0 @@
-.\segger_rtt.o: C:\Nordic Semiconductor\external\segger_rtt\SEGGER_RTT.c
-.\segger_rtt.o: C:\Nordic Semiconductor\external\segger_rtt\SEGGER_RTT.h
-.\segger_rtt.o: C:\Nordic Semiconductor\external\segger_rtt\SEGGER_RTT_Conf.h
-.\segger_rtt.o: .\Include\compiler_abstraction.h
-.\segger_rtt.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
